b2e884207ca9227bb510f1ceb4a78dd59d72568c
kuanhmn <thanhbinhpt95@gmail.com>
2025-10-06 07:20:22 +0700

    Update run.sh

--- gti-control/run.sh
+++ gti-control/run.sh
@@ -3,5 +3,3 @@ set -e
 export PYTHONPATH=/app:${PYTHONPATH}
 echo "[gti] starting GTI Control (Ingress UI)"
 exec uvicorn server:app --host 0.0.0.0 --port 8099
-COPY run.sh /run.sh
-RUN sed -i 's/\r$//' /run.sh && chmod a+x /run.sh
